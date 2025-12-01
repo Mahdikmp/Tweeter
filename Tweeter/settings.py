26c6939fd2c49d8e09f263f80424aeca5cfa9e67
Mahdikmp <kmpmahdi1381@gmail.com>
2025-12-01 16:02:22 +0330

    Fix following page appeariance

--- Tweeter/settings.py
+++ Tweeter/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@fe+c@nv)xh&h!870(#m#_shd0*ty&birikr+%ju0(5qy9+3i='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'web-production-25950.up.railway.app',
@@ -111,10 +111,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = []
 
